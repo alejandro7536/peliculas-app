1d6559e0a72ef389fd3291b64910e4e1edd7c2f9
alejandro7536 <alejandroalas7536@gmail.com>
2023-01-21 12:29:34 -0600

    custom tag

--- src/app/components/peliculas-poster-grid/peliculas-poster-grid.component.ts
+++ src/app/components/peliculas-poster-grid/peliculas-poster-grid.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, HostListener } from '@angular/core';
 import { Movie } from '../../interfaces/cartelera-response';
 import { PeliculasService } from '../../services/peliculas.service';
 import { Router } from '@angular/router';
+import { GoogleTagManagerService } from 'angular-google-tag-manager';
 @Component({
   selector: 'app-peliculas-poster-grid',
   templateUrl: './peliculas-poster-grid.component.html',
@@ -12,7 +13,8 @@ export class PeliculasPosterGridComponent implements OnInit {
   @Input() movies: Movie[];
 
   constructor(
-    private router: Router
+    private router: Router,
+    private gtm: GoogleTagManagerService
   ) { }
 
   ngOnInit(): void {
@@ -22,7 +24,21 @@ export class PeliculasPosterGridComponent implements OnInit {
 
   onClick(movie: Movie) {
     this.router.navigate(['/pelicula', movie.id]);
+    console.log('Movie', movie);
+    this.sendTag(movie);
+  }
 
+  sendTag(movie: Movie) {
+    this.gtm.pushTag({
+      "event": "viewMovie",
+      "content": {
+        "title": movie.title,
+        "vote_average": movie.vote_average,
+        "release_date": movie.release_date,
+        "popularity": movie.popularity,
+        "adult": movie.adult
+      }
+    })
   }
 
 }
